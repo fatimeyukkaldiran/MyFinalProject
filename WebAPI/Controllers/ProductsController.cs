0b8d44f9ac3a0ba345455a2d4fdfda7251f0ccd6
fatimeyukkaldiran <fatime.yukkaldiran8@gmail.com>
2021-02-13 22:59:52 +0300

    working with Web API

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")] //attribute in c#, 
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //naming convention
+        IProductService _productService; //field
+
+        //loosely coupled
+        //IoC container -- Inversion of control
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            //Dependency chain
+           
+            var result = _productService.GetAll();
+            return result.Message;
+        }
+    }
+}
